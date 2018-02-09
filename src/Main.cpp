0b3ca87c9e8da88830980e76bf3f0b3b31a3e3aa
Caio Toledo <caio_toledo_2@hotmail.com>
2018-02-09 00:44:36 -0200

    Showing more information in main test

--- src/Main.cpp
+++ src/Main.cpp
@@ -15,28 +15,39 @@
 
 #define SIZE_TREE   1000000
 
+void printSearch(BinaryTree *t, mydata d){
+
+  int start_s=clock();
+
+  std::cout << "Number " << d;
+  if ( !t->Search(d) ) {
+    std::cout << " NOT";
+  }
+  std::cout << " found!" << std::endl;
+
+  std::cout << "Search Time: " << (clock()-start_s)/double(CLOCKS_PER_SEC)*1000 << " ms" << std::endl;
+}
+
 int main() {
 	std::cout << "Hello World" << std::endl << std::endl; // prints Hello World
 
 	BinaryTree tree;
 
+	int start_s=clock();
+
   for (int i = 0; i < SIZE_TREE; ++i) {
     tree.Insert(rand() % SIZE_TREE);
   }
 
-  tree.SaveBinaryTree();
-	tree.LoadBinaryTree();
+  std::cout << SIZE_TREE << " Data Insertions in " << (clock()-start_s)/double(CLOCKS_PER_SEC)*1000 << " ms." << std::endl;
 
+  std::cout << std::endl;
+  printSearch(&tree, rand() % SIZE_TREE);
+
+  std::cout << std::endl << "Binary Tree Infos:" << std::endl;
   std::cout << "Min Value " << tree.MinValue() << std::endl;
   std::cout << "Max Value " << tree.MaxValue() << std::endl;
   std::cout << "Height " << tree.Height() << std::endl;
-
-  int start_s=clock();
-
-	std::cout << tree.Search(18) << std::endl;
-
-  int stop_s=clock();
-  std::cout << "Search Time: " << (stop_s-start_s)/double(CLOCKS_PER_SEC)*1000 << " ms" << std::endl;
 
   tree.SaveBinaryTree();
 
